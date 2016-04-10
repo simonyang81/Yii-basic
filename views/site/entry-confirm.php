cb69f09768cc66f13ce9e730cb89430da92b8e3d
simonyang81 <simonyang81@gmail.com>
2016-04-10 20:47:04 +0800

    isset & empty

--- views/site/entry-confirm.php
+++ views/site/entry-confirm.php
@@ -7,3 +7,21 @@ use yii\helpers\Html;
     <li><label>Name</label>: <?= Html::encode($model->name) ?></li>
     <li><label>Email</label>: <?= Html::encode($model->email) ?></li>
 </ul>
+
+<?php
+
+$username = Html::encode($model->name);
+$email = Html::encode($model->email);
+
+?>
+
+<br>
+<p>测试isset和empty:</p>
+<ul>
+    <?php
+        echo '<li><label>isset($username):</label>'.isset($username).'</li>';
+        echo '<li><label>isset($nothere):</label>'.isset($nothere).'</li>';
+        echo '<li><label>empty($username):</label>'.empty($username).'</li>';
+        echo '<li><label>empty($nothere):</label>'.empty($nothere).'</li>';
+    ?>
+</ul>
